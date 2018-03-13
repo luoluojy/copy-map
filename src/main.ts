6266f4627f466a4d11bebe09cb2717756866bc74
luoluo21 <luoluojy@gmail.com>
2018-03-13 13:11:40 +0800

    dist

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-window['CESIUM_BASE_URL'] = '/assets/cesium';
+window['CESIUM_BASE_URL'] = '/assets/plugin/cesium';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
