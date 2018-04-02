bb8601f09a66661fbce59f2fe06ac5a7c8aebc2e
ZhangHong <geothinking@126.com>
2018-04-02 18:22:00 +0800

    更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,54 +14,45 @@ export class AppComponent implements OnInit {
 
   constructor(private elementRef: ElementRef, private initCesiumService: InitCesiumService) { }
 
-<<<<<<< HEAD
-  }
-
-=======
->>>>>>> fd0a63570a55f7a09e6f43ede42d6657d24a78c6
   ngOnInit() {
     this.initCesiumService.initCesium();
     this.blurElements();
   }
   shown: boolean = false;
   actionShown: boolean = false;
-  collapseShown:boolean=false;
+  collapseShown: boolean = false;
   topShown: boolean = false;
   viewState: boolean = false;
-  menuBarShown:boolean = true;
-  recMenuBarState(event){
+  menuBarShown: boolean = true;
+  recMenuBarState(event) {
     this.menuBarShown = event;
   }
 
-<<<<<<< HEAD
-=======
-  
   recShown(event) {
     this.shown = event;
   }
-  recActionShown(event){
+  recActionShown(event) {
     this.actionShown = event;
     this.collapseShown = event;
   }
   recViewState(event) {
     this.viewState = event;
   }
-  
-  recCollapseState(event){
+
+  recCollapseState(event) {
     this.collapseShown = event
   }
 
-  modalShown:boolean = false;
-  recCreateProjectState(event){
+  modalShown: boolean = false;
+  recCreateProjectState(event) {
     this.modalShown = event;
   }
 
-  recModalShown(event){
-    this.modalShown=event;
-    this.shown=!event;
+  recModalShown(event) {
+    this.modalShown = event;
+    this.shown = !event;
   }
-  
->>>>>>> fd0a63570a55f7a09e6f43ede42d6657d24a78c6
+
   blurElements() {
     let elements = document.querySelectorAll('.unfocus');
     for (let i = 0; i < elements.length; i++) {
