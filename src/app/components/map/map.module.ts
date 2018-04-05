a221110bd3d6f286ab325a54d71d10b23804a7d4
ZhangHong <geothinking@126.com>
2018-04-05 21:00:47 +0800

    完善

--- src/app/components/map/map.module.ts
+++ src/app/components/map/map.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OpenlayersComponent } from './openlayers/openlayers.component';
+import { CesiumComponent } from './cesium/cesium.component';
+import { OpenlayersService } from './openlayers.service';
+import { CesiumService } from './cesium.service';
+import { MapService } from './map.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    OpenlayersComponent,
+    CesiumComponent
+  ],
+  providers:[
+    OpenlayersService,
+    CesiumService,
+    MapService
+  ]
 })
 export class MapModule { }
