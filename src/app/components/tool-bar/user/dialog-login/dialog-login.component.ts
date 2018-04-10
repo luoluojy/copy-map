4d5abe66eec8753634266f562a61c4183d45d78a
程立龙 <luoluojy@hotmail.com>
2018-04-10 18:38:47 +0800

    增加登录对话框

--- src/app/components/tool-bar/user/dialog-login/dialog-login.component.ts
+++ src/app/components/tool-bar/user/dialog-login/dialog-login.component.ts
@@ -21,7 +21,6 @@ export class DialogLoginComponent implements OnInit {
   login(value){
     let username = value['login'];
     let password = value['password'];
-    console.log(username,password);
     this.http.post('http://58.213.133.181:7774/api2/auth-token/',JSON.stringify({
       username:username,
       password:password
