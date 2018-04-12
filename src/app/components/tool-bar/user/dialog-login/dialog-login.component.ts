b08a4b2ab69a5b5f87100c8fd51b311d1d3d00d7
程立龙 <luoluojy@hotmail.com>
2018-04-12 10:03:18 +0800

    尝试解决跨域问题

--- src/app/components/tool-bar/user/dialog-login/dialog-login.component.ts
+++ src/app/components/tool-bar/user/dialog-login/dialog-login.component.ts
@@ -204,7 +204,7 @@ export class DialogLoginComponent implements OnInit {
     let password = value["password"];
     this.http
       .post(
-        "/api2/auth-token/",
+        environment.http_href + "/api2/auth-token/",
         "username=" + username + "&&password=" + password,
         {
           headers: new HttpHeaders({
@@ -219,6 +219,7 @@ export class DialogLoginComponent implements OnInit {
             alert("登录成功" + value["token"]);
             this.http
               .get(
+                environment.http_href +
                   "/api2/avatars/user/" +
                   username +
                   "/resized/80/",
